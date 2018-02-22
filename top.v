9483fb6f3fa7b6ac5d39a5a280bbc496dc3f59e6
TonEnfer <tonik.sam@gmail.com>
2018-02-22 17:27:50 +0500

    Всё работает

--- top.v
+++ top.v
@@ -8,14 +8,13 @@ module top(
 	counter c(clk,qz,count);
 	always @(posedge clk or posedge qz)
 	begin
-		if(qz)
+		if(qz == 1)
 			out = 0;
 		else begin
-				if((count >= 125 && count < 250)||(count >=375))
-					out = 1;
-				else
-					out = 0;
+			if((count >= 250 && count < 500)||(count >=750))
+				out = 1;
+			else
+				out = 0;
 		end
-		
 	end
 endmodule
